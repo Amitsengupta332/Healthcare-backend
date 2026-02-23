4cf89546c3f15d4794dc611786795adddd5b701b
amitsengupta <amitsengupta332@gmail.com>
2026-02-24 00:31:31 +0600

    Creating Send Response Utility Functions And Setting Up Multi File Prisma Schema Setup

--- src/app/module/specialty/specialty.controller.ts
+++ src/app/module/specialty/specialty.controller.ts
@@ -1,39 +1,34 @@
 import { Request, Response } from "express";
 import { SpecialtyService } from "./specialty.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
-// const createSpecialty = async (req: Request, res: Response) => {
-//   const payload = req.body;
-//   const specialty = await SpecialtyService.createSpecialty(payload);
-//   res.status(201).json({
-//     success: true,
-//     message: "Specialty created successfully",
-//     data: specialty,
-//   });
-// };
 const createSpecialty = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
-  const specialty = await SpecialtyService.createSpecialty(payload);
-  res.status(201).json({
+  const result = await SpecialtyService.createSpecialty(payload);
+  sendResponse(res, {
+    httpStatusCode: 201,
     success: true,
     message: "Specialty created successfully",
-    data: specialty,
+    data: result,
   });
 });
 
 const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
-  const specialties = await SpecialtyService.getAllSpecialties();
-  res.status(200).json({
+  const result = await SpecialtyService.getAllSpecialties();
+  sendResponse(res, {
+    httpStatusCode: 200,
     success: true,
     message: "Specialties fetched successfully",
-    data: specialties,
+    data: result,
   });
 });
 
 const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await SpecialtyService.deleteSpecialty(id as string);
-  res.status(200).json({
+  sendResponse(res, {
+    httpStatusCode: 200,
     success: true,
     message: "Specialty deleted successfully",
     data: result,
