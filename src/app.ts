e8ca4af749dea551253851da440abd48f01d4dd4
amitsengupta <amitsengupta332@gmail.com>
2026-02-24 22:04:00 +0600

    Creating Global Error Handler And Not Found

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { prisma } from "./app/lib/prisma";
 import { IndexRoutes } from "./app/routes";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 // import { prisma } from "./app/lib/prisma";
 
 const app: Application = express();
@@ -29,18 +31,8 @@ app.get("/", async (req: Request, res: Response) => {
   });
 });
 
-// Basic route
-// app.get("/", async (req: Request, res: Response) => {
-//   const specialty = await prisma.specialty.create({
-//       data: {
-//           title: 'Cardiology'
-//       }
-//   })
-//   res.status(201).json({
-//       success: true,
-//       message: 'API is working',
-//       data: specialty
-//   })
-// });
+
+app.use(globalErrorHandler);
+app.use(notFound)
 
 export default app;
