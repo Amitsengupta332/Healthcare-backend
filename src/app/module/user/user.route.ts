92928e111a6aeb6b60e1ef5cbc901386cb369ce5
amitsengupta <amitsengupta332@gmail.com>
2026-02-26 01:37:20 +0600

    Creating Zod Schema And Validating , Sanitizing, Parsing Zod Schema With User Input Request Body

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,9 +1,28 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createDoctorZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/create-doctor", UserController.createDoctor);
+router.post("/create-doctor",
+       //     (req: Request, res: Response, next: NextFunction) => {
+
+    //     const parsedResult = createDoctorZodSchema.safeParse(req.body);
+
+    //     if (!parsedResult.success) {
+    //         next(parsedResult.error)
+    //     }
+
+    //     //sanitizing the data
+    //     req.body = parsedResult.data;
+
+    //     next()
+
+    // }, 
+
+    validateRequest(createDoctorZodSchema),
+     UserController.createDoctor);
 
 //Todo : implement admin and superAdmin later
 // router.post("/create-admin", UserController.createDoctor);
