3a8492fb149b78a448e5ce7f6b18e6739e459e32
amitsengupta <amitsengupta332@gmail.com>
2026-02-27 01:23:58 +0600

    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
- 
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma";
 
 const router = Router();
 
-router.post("/", SpecialtyController.createSpecialty);
+router.post(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  SpecialtyController.createSpecialty,
+);
 router.get("/", SpecialtyController.getAllSpecialties);
 router.delete("/:id", SpecialtyController.deleteSpecialty);
 
