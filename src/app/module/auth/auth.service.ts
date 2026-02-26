9aa03539a6758325c006f4f4daf5c915b137a36b
amitsengupta <amitsengupta332@gmail.com>
2026-02-26 21:18:38 +0600

    Creating Access Token And Refresh Token With JWT During Login User And Patient Registration

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UserStatus } from "../../../generated/prisma";
 import AppError from "../../errorHelpers/AppError";
 import { auth } from "../../lib/auth";
 import { prisma } from "../../lib/prisma";
+import { tokenUtils } from "../../utils/token";
 
 interface IRegisterPatientPayload {
   name: string;
@@ -72,15 +73,6 @@ const loginUser = async (payload: ILoginUserPayload) => {
       password,
     },
   });
-
-  // if (data.user.status == UserStatus.BLOCKED) {
-  //   throw new Error("User is blocked");
-  // }
-
-  // if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
-  //   throw new Error("User is deleted");
-  // }
-
   if (data.user.status === UserStatus.BLOCKED) {
     throw new AppError(status.FORBIDDEN, "User is blocked");
   }
@@ -89,7 +81,29 @@ const loginUser = async (payload: ILoginUserPayload) => {
     throw new AppError(status.NOT_FOUND, "User is deleted");
   }
 
-  return data;
+  const accessToken = tokenUtils.getAccessToken({
+    userId: data.user.id,
+    role: data.user.role,
+    name: data.user.name,
+    email: data.user.email,
+    status: data.user.status,
+    isDeleted: data.user.isDeleted,
+    emailVerified: data.user.emailVerified,
+  });
+  const refreshToken = tokenUtils.getRefreshToken({
+    userId: data.user.id,
+    role: data.user.role,
+    name: data.user.name,
+    email: data.user.email,
+    status: data.user.status,
+    isDeleted: data.user.isDeleted,
+    emailVerified: data.user.emailVerified,
+  });
+  return {
+    ...data,
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const AuthService = {
